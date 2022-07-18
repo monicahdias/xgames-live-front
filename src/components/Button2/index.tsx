a619a819912f0e7daa1b288a800f652e9e226d49
monicashdias <msumiedias@gmail.com>
2022-07-18 20:52:45 -0300

    create Button2

---
+++ src/components/Button2/index.tsx
@@ -0,0 +1,14 @@
+import React, {ButtonHTMLAttributes} from "react";
+import * as S from "./style";
+
+type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>;
+
+type ButtonProps = {
+  value: string;
+} & ButtonType;
+
+const Button2 = ({value, ...props}: ButtonProps) => {
+  return <S.Button2 {...props}>{value}</S.Button2>;
+};
+
+export default Button2;
---
+++ src/components/Button2/style.ts
@@ -0,0 +1,23 @@
+import styled, {css} from "styled-components";
+
+export const Button2 = styled.button`
+  ${({theme}) => css`
+    background: ${theme.colors.secondaryColor};
+    border: none;
+    border-radius: 6px;
+    color: ${theme.colors.textColor};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 2.7rem;
+    width: 100%;
+    cursor: pointer;
+    ${theme.mixins.buttonStyle()}
+    font-weight: 700;
+    &:hover {
+      color: ${theme.colors.secondaryColor};
+      background: ${theme.colors.textButton};
+      box-shadow: none;
+    }
+  `}
+`;
