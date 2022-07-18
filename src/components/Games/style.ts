1986909b5d8dc0cc560f2cc170c3b88fd9cf2862
monicashdias <msumiedias@gmail.com>
2022-07-18 20:53:18 -0300

    create Games

---
+++ src/components/Games/style.ts
@@ -0,0 +1,10 @@
+import styled, {css} from "styled-components";
+
+export const Games = styled.section`
+  ${() => css`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin: 1rem;
+  `}
+`;
