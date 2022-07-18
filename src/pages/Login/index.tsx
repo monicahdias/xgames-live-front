ca06a22e76f13c65aa941189680c5dba741e14f6
monicashdias <msumiedias@gmail.com>
2022-07-18 20:53:56 -0300

    create Login page

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,14 @@
+import LoginBox from "components/LoginBox";
+import * as S from "./style";
+
+const Login = () => {
+  return (
+    <S.Login>
+      <S.LoginContent>
+        <LoginBox />
+      </S.LoginContent>
+    </S.Login>
+  );
+};
+
+export default Login;
