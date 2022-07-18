ced2fd0173502838e81f281e7793b9c0653fdc8a
monicashdias <msumiedias@gmail.com>
2022-07-18 20:46:17 -0300

    creating RoutePath

---
+++ src/types/routes.ts
@@ -0,0 +1,14 @@
+export enum RoutePath {
+  HOME = "/",
+  LOGIN = "/login",
+  USER = "/user",
+  USER_CREATE = "/user-create",
+  USER_EDIT = "/user-edit",
+  PROFILES = "/profiles",
+  PROFILE_CREATE = "/profile-create",
+  PROFILE_EDIT = "/profile-edit-${id}",
+  PROFILE_HOME = "/profile-${id}",
+  GAME_CREATE = "/game-create",
+  GAME_EDIT = "/game-edit-${id}",
+  GAME = "/game-${id}",
+}
