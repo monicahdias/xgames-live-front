d70dc44316a058227981d2ead0728a96a8d70846
monicashdias <msumiedias@gmail.com>
2022-07-18 20:53:29 -0300

    create LoginBox

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,65 @@
+import * as S from "./style";
+import logo from "assets/imgs/logo.png";
+import Button1 from "components/Button1";
+import Button2 from "components/Button2";
+import {useState} from "react";
+
+interface userLogin {
+  email: string;
+  password: string;
+}
+
+const LoginBox = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChangesValues = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const nameElement = event.target.name;
+    const typedElement = event.target.value;
+
+    setValues((values: userLogin) => {
+      return {
+        ...values,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+
+  const loginUser = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    console.log(values);
+  };
+
+  return (
+    <S.LoginBox>
+      <S.LoginBoxLogo>
+        <S.LoginBoxLogoImg src={logo} alt="logo" />
+      </S.LoginBoxLogo>
+      <S.LoginBoxForm onSubmit={loginUser}>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="E-mail"
+          onChange={handleChangesValues}
+          autoComplete="false"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          onChange={handleChangesValues}
+        />
+        <S.Buttons>
+          <Button1 value="LOG IN" type="submit" />
+          <Button2 value="REGISTER" type="button" />
+        </S.Buttons>
+      </S.LoginBoxForm>
+    </S.LoginBox>
+  );
+};
+
+export default LoginBox;
---
+++ src/components/LoginBox/style.ts
@@ -0,0 +1,60 @@
+import styled, {css} from "styled-components";
+
+export const LoginBox = styled.section`
+  ${({theme}) => css`
+    background-color: ${theme.colors.baseBg2};
+    width: 330px;
+    height: 414px;
+    padding: 20px 20px 35px 20px;
+    margin: 20px;
+    border-radius: 6px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    box-shadow: 3px 3px 6px 3px ${theme.colors.primaryColorShadow};
+    color: ${theme.colors.textColor};
+  `}
+`;
+
+export const LoginBoxLogo = styled.div`
+  ${() => css`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-bottom: 30px;
+  `}
+`;
+
+export const LoginBoxLogoImg = styled.img`
+  ${() => css`
+    width: 230px;
+  `}
+`;
+
+export const LoginBoxForm = styled.form`
+  ${({theme}) => css`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    input {
+      ${theme.mixins.input()};
+    }
+  `}
+`;
+
+export const Buttons = styled.div`
+  ${({theme}) => css`
+    background-color: ${theme.colors.baseBg1};
+    width: 10rem;
+    margin: 0;
+    border: none;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    align-self: center;
+    gap: 0.8rem;
+    margin-top: 0.8rem;
+  `}
+`;
