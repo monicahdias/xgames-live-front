288177f3600265ed28322225ef9f3a7524b25093
monicashdias <msumiedias@gmail.com>
2022-07-19 19:38:56 -0300

    create component GameList

---
+++ src/components/GameList/index.tsx
@@ -0,0 +1,12 @@
+import GameCard from "components/GameCard";
+import * as S from "./style";
+
+const GameList = () => {
+  return (
+    <S.GameList>
+      <GameCard />
+    </S.GameList>
+  );
+};
+
+export default GameList;
---
+++ src/components/GameList/style.ts
@@ -0,0 +1,10 @@
+import styled, {css} from "styled-components";
+
+export const GameList = styled.section`
+  ${() => css`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin: 1rem;
+  `}
+`;
